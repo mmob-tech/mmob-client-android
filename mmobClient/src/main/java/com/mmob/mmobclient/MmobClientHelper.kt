0fb9000502207a2ee98468d2916535bc3948149b
Hasseeb Hussain <hasseeb473@gmail.com>
2023-05-10 15:15:52 +0100

    Add client helper class

--- mmobClient/src/main/java/com/mmob/mmobclient/MmobClientHelper.kt
+++ mmobClient/src/main/java/com/mmob/mmobclient/MmobClientHelper.kt
@@ -1,4 +1,29 @@
 package com.mmob.mmobclient
 
+import android.net.Uri
+import com.google.common.net.InternetDomainName
+import java.net.URI
+
 class MmobClientHelper {
+    fun getDomain(uri: Uri): String? {
+        return try {
+            val uriHostString = uri.host.toString()
+            InternetDomainName.from(uriHostString).topPrivateDomain().toString()
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    fun getHost(urlString: String?): String? {
+        return try {
+            if (urlString == null) {
+                return null
+            }
+
+            val uri = URI(urlString)
+            uri.host
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
