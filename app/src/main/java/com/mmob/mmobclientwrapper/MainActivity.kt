0cb94f07e5b58fa06bf047841537c3350f0fafdf
Hasseeb Hussain <hasseeb473@gmail.com>
2023-08-29 11:38:35 +0100

    Temp fix, prompt user with option to proceed unsafely

--- app/src/main/java/com/mmob/mmobclientwrapper/MainActivity.kt
+++ app/src/main/java/com/mmob/mmobclientwrapper/MainActivity.kt
@@ -2,10 +2,37 @@ package com.mmob.mmobclientwrapper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mmob.mmobclient.InstanceDomain
+import com.mmob.mmobclient.MmobClient
+import com.mmob.mmobclient.MmobView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mmobView: MmobView = findViewById(R.id.mmob_view)
+        val client = MmobClient(mmobView, applicationContext, instanceDomain = InstanceDomain.EFNETWORK)
+
+        val integration = MmobClient.MmobIntegrationConfiguration(
+            cp_id = "cp_TMaiVSyyzBx2rZqF-PqdY",
+            cp_deployment_id = "cpd_xH1KQOhFh_hIIVKTCBJF5",
+            environment = "stag"
+        )
+
+        val customerInfo = MmobClient.MmobCustomerInfo(
+            customerInfo = MmobClient.MmobCustomerInfo.Configuration(
+                email = "john.smith@example.com",
+                first_name = "John",
+                surname = "Smith",
+                title = "Mr",
+                building_number = "Flat 81",
+                address_1 = "Marconi Square",
+                town_city = "Chelmsford",
+                postcode = "CM1 1XX"
+            )
+        )
+
+        client.loadIntegration(integration = integration, customerInfo = customerInfo)
     }
 }
--- mmobClient/src/main/java/com/mmob/mmobclient/MmobBrowser.kt
+++ mmobClient/src/main/java/com/mmob/mmobclient/MmobBrowser.kt
@@ -1,18 +1,24 @@
 package com.mmob.mmobclient
 
 import android.annotation.TargetApi
+import android.net.http.SslError
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.webkit.SslErrorHandler
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MmobBrowser : AppCompatActivity() {
+    private var userProceededUnsafely = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mmob_browser)
@@ -47,6 +53,57 @@ class MmobBrowser : AppCompatActivity() {
                 request: WebResourceRequest
             ): Boolean {
                 return handleOverrideUrlLoading(view, request.url.toString())
+            }
+
+            override fun onReceivedSslError(
+                view: WebView?,
+                handler: SslErrorHandler,
+                error: SslError
+            ) {
+                if (userProceededUnsafely) {
+                    return handler.proceed()
+                }
+
+                var errorString = ""
+
+                when (error.primaryError) {
+                    SslError.SSL_UNTRUSTED -> errorString = "the certificate authority is not trusted."
+                    SslError.SSL_EXPIRED -> errorString = "the certificate has expired."
+                    SslError.SSL_IDMISMATCH -> errorString = "the certificate has a hostname mismatch."
+                    SslError.SSL_NOTYETVALID -> errorString = "the certificate is not valid."
+                }
+
+                val builder = AlertDialog.Builder(this@MmobBrowser)
+                builder.setTitle("SSL Certificate Error")
+                builder.setMessage(
+                        "We couldn't establish a secure connection to the website due to an SSL certificate error. " +
+                        "This was because $errorString\n\n" +
+                        "Your security is important to us. To ensure your safety, we recommend not proceeding to this website unless you trust its source."
+                )
+
+                builder.setPositiveButton("Go back") { _, _ ->
+                    handler.cancel()
+
+                    // If user can go back in webView, do so, else close the in app browser
+                    if (view != null && view.canGoBack()) {
+                        view.goBack()
+                    } else {
+                        finish()
+                    }
+
+                    Toast.makeText(this@MmobBrowser,
+                        "Returning to previous page...", Toast.LENGTH_SHORT).show()
+                }
+
+                builder.setNegativeButton("Proceed unsafely") { _, _ ->
+                    handler.proceed()
+                    userProceededUnsafely = true
+
+                    Toast.makeText(this@MmobBrowser,
+                        "Proceeding unsafely...", Toast.LENGTH_SHORT).show()
+                }
+
+                builder.show()
             }
 
             private fun handleOverrideUrlLoading(view: WebView?, url: String?): Boolean {
@@ -85,8 +142,8 @@ class MmobBrowser : AppCompatActivity() {
                 }
 
                 if (!webView.canGoForward()) {
-                    forwardButton.setImageResource(R.drawable.ic_arrow_forward_disabled)
                     forwardButton.isClickable = false
+                    forwardButton.setImageResource(R.drawable.ic_arrow_forward_disabled)
                 } else {
                     forwardButton.isClickable = true
                     forwardButton.setImageResource(R.drawable.ic_arrow_forward)
