76db1d642b4577e033b6a8bb59f68a2c47833e0a
Hasseeb Hussain <hasseeb473@gmail.com>
2023-08-31 15:56:27 +0100

    Update helper class with methods, extract constants

--- mmobClient/src/main/java/com/mmob/mmobclient/MmobClientHelper.kt
+++ mmobClient/src/main/java/com/mmob/mmobclient/MmobClientHelper.kt
@@ -1,5 +1,7 @@
 package com.mmob.mmobclient
 
+import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import com.google.common.net.InternetDomainName
 import java.net.URI
@@ -9,18 +11,39 @@ enum class InstanceDomain {
 }
 
 class MmobClientHelper {
-    fun containsAffiliateRedirect(urlString: String): Boolean {
-        return "affiliate-redirect" in urlString
+    object Constants {
+        const val AFFILIATE_REDIRECT_PATH = "affiliate-redirect"
+        const val MMOB_ROOT_DOMAIN = "mmob.com"
+        const val EFNETWORK_ROOT_DOMAIN = "ef-network.com"
+        val BLACKLISTED_DOMAINS = arrayOf("play.google.com", "appgallery.huawei.com")
     }
 
-    fun getHost(urlString: String?): String? {
-        return try {
-            if (urlString == null) {
-                return null
-            }
+    fun isAffiliateRedirect(uri: Uri): Boolean {
+        val uriString = uri.toString()
+        return Constants.AFFILIATE_REDIRECT_PATH in uriString
+    }
 
-            val uri = URI(urlString)
-            uri.host
+    fun isBlacklistedDomain(uri: Uri): Boolean {
+        return Constants.BLACKLISTED_DOMAINS.contains(this.getHost(uri))
+    }
+
+    fun isValidUrlScheme(uri: Uri): Boolean {
+        val uriString = uri.toString()
+        return uriString.startsWith("http://") || uriString.startsWith("https://")
+    }
+
+    fun isUriValid(uriString: String): Boolean {
+        return try {
+            val uri = URI(uriString)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun getHost(uri: Uri): String? {
+        return try {
+            return uri.host
         } catch (e: Exception) {
             null
         }
@@ -28,8 +51,8 @@ class MmobClientHelper {
 
     fun getInstanceDomain(instanceDomain: InstanceDomain): String {
         return when (instanceDomain) {
-            InstanceDomain.MMOB -> "mmob.com"
-            InstanceDomain.EFNETWORK -> "ef-network.com"
+            InstanceDomain.MMOB -> Constants.MMOB_ROOT_DOMAIN
+            InstanceDomain.EFNETWORK -> Constants.EFNETWORK_ROOT_DOMAIN
         }
     }
 
@@ -40,5 +63,14 @@ class MmobClientHelper {
         } catch (e: Exception) {
             null
         }
+    }
+
+    fun openUriInBrowser(context: Context, uri: Uri): Boolean {
+        if (isUriValid(uri.toString())) {
+            val browserIntent = Intent(Intent.ACTION_VIEW, uri)
+            context.startActivity(browserIntent)
+            return true
+        }
+        return false
     }
 }
