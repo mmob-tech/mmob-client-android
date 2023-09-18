23860d1264d6a77af8154e171c378446e7ae5743
Hasseeb Hussain <hasseeb473@gmail.com>
2023-09-18 10:05:18 +0100

    Fix code smell I think

--- mmobClient/src/main/java/com/mmob/mmobclient/MmobClientHelper.kt
+++ mmobClient/src/main/java/com/mmob/mmobclient/MmobClientHelper.kt
@@ -17,6 +17,7 @@ class MmobClientHelper {
         const val AFFILIATE_REDIRECT_PATH = "affiliate-redirect"
         const val MMOB_ROOT_DOMAIN = "mmob.com"
         const val EFNETWORK_ROOT_DOMAIN = "ef-network.com"
+        const val LOCAL_HOST_DOMAIN = "10.0.2.2"
 
         // Domains we want opened using native URL scheme (aka Chrome)
         val BLACKLISTED_DOMAINS = arrayOf("play.google.com", "appgallery.huawei.com")
@@ -29,7 +30,7 @@ class MmobClientHelper {
             val host = url.host
             val port = url.port
 
-            host == "10.0.2.2" && port == 3100
+            host == Constants.LOCAL_HOST_DOMAIN && port == 3100
         } catch (e: MalformedURLException) {
             false
         }
